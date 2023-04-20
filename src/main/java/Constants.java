ef293bfda632382740449a12a069c3a6b65e5a48
Rustam Ilin <rustam.ilin@aisa.ru>
2023-04-20 16:49:00 +0300

    begin

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String GIRL_FILE_NAME = "girl.txt";
     public static final String MAN_FILE_NAME = "man.txt";
-    public static final String FILE_CITY ="city";
+    public static final String FILE_CITY ="city.txt";
 
-    public static final String FILE_STREET ="street";
+    public static final String FILE_STREET ="street.txt";
 }
